233f041a44ed10f597059905e2ca436d6915cd12
nb341153 <nik.bo@hotmail.de>
2014-01-09 20:41:32 +0100

    changed structure in main

--- Main.cpp
+++ Main.cpp
@@ -5,22 +5,14 @@
  *      Author: David
  */
 #include <QApplication>
-#include"Konstant.h"
-#include"Brett.h"
-#include"Game.h"
-#include"KIspieler.h"
-#include"testKI.h"
-#include"MyFirstKI.h"
-#include<iostream>
+#include "uiexec.h"
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
 	QApplication a(argc, argv);
-	Brett board;
-	board.Ausgabe();
-	testKI virt[] = { testKI(-1), testKI(-2) };
-	Game game1(2, 0, virt, board);
-	game1.spielen();
+  UIEXEC uiexec;
+
 	return a.exec();
 }
 /*
