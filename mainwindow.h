049b7637f670135292443b7e8e7a85544ae8f2bf
nb341153 <nik.bo@hotmail.de>
2014-01-16 18:45:07 +0100

    added QMainWindow

---
+++ mainwindow.h
@@ -0,0 +1,17 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+#include "uiexec.h"
+#include "window.h"
+
+class MainWindow : QMainWindow
+{
+public:
+    MainWindow();
+private:
+    UIEXEC* myGameExe;
+    Window* wp;
+    QToolBar* myToolBar;
+};
+
+#endif // MAINWINDOW_H
