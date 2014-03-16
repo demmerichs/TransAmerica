2e471e7ba404ec29598c900b5e9c5b0dcf90c0ad
OEtzi007 <DavidSchmidt314@gmail.com>
2014-03-16 17:37:21 +0100

    made it working with .o files, still not very professional
    Signed-off-by: OEtzi007 <DavidSchmidt314@gmail.com>

---
+++ src/ai/DavidAI.cpp
@@ -0,0 +1,40 @@
+/*
+ * DavidAI.cpp
+ *
+ *  Created on: 15.03.2014
+ *      Author: David
+ */
+
+//==============================
+// forward declared dependencies
+//==============================
+// included dependencies
+#include "../../hdr/ai/DavidAI.h"
+
+#include "../../hdr/game/Constants.h"
+#include "../../hdr/game/Move.h"
+#include "../../hdr/game/Pawn.h"
+//==============================
+// the actual code
+
+DavidAI::DavidAI(PLAYERCOLOR playerColor) :
+		AI(playerColor) {
+}
+
+DavidAI::~DavidAI() {
+}
+
+Move DavidAI::doMove(State &currentState, vector<Move*> moveList) {
+
+}
+
+const Coordinate* DavidAI::setPawn(State &currentState) {
+}
+
+bool DavidAI::countPoints(State& currentState,
+		vector<Connection*>& returnPath) {
+}
+
+void DavidAI::gatherInformationEndOfRound(
+		const RoundLogger* informationAboutGame) {
+}
