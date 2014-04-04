fdf520e4dbe9c4faebec4c444592ce5428b1b407
OEtzi007 <DavidSchmidt314@gmail.com>
2014-04-04 15:16:06 +0200

    debugged and updated documentation

--- src/userinterface/GUIBoard.cpp
+++ src/userinterface/GUIBoard.cpp
@@ -33,14 +33,14 @@ GUIBoard::GUIBoard(const Board* board, Counter points,
 				deadLine), selectConnections(false), selectCoordinates(false) {
 	drawCity = false;
 
-    setBackgroundRole(QPalette::Dark);
+	setBackgroundRole(QPalette::Dark);
 	setAutoFillBackground(false);
 	setMouseTracking(true);
 
-    background = new QPixmap("images/tbg1.png");
+	background = new QPixmap("images/tbg1.png");
 
-    transform.translate(110.5*2, 57.5*2);
-    transform.scale(1*2, sqrt(3)/2.*1.95);
+	transform.translate(110.5 * 2, 57.5 * 2);
+	transform.scale(1 * 2, sqrt(3) / 2. * 1.95);
 	transform.shear(-0.5, 0);
 	invertedTransform = transform.inverted();
 	this->update();
@@ -71,7 +71,7 @@ void GUIBoard::paintEvent(QPaintEvent*) {
 	if (drawCity)
 		drawCityNames(&painter);
 	drawRat(&painter);
-    drawPoints(&painter);
+	drawPoints(&painter);
 	painter.setWorldTransform(scale.inverted(), true);
 }
 
@@ -160,7 +160,7 @@ void GUIBoard::drawGrid(QPainter* painter) {
 	for (int i = 0; i < MAX_X; i++) {
 		for (int j = 0; j < MAX_Y; j++) {
 			if (dynamicState->board.edges[i][j][0]) {
-				if ((dynamicState->board.edges[i][j][0])->hindernis) {
+				if ((dynamicState->board.edges[i][j][0])->barrier) {
 					painter->setPen(thinRedPen);
 				} else
 					painter->setPen(thinPen);
@@ -168,7 +168,7 @@ void GUIBoard::drawGrid(QPainter* painter) {
 						transform.map(QPoint(i * sL + sL, j * sL)));
 			}
 			if (dynamicState->board.edges[i][j][2]) {
-				if (dynamicState->board.edges[i][j][2]->hindernis) {
+				if (dynamicState->board.edges[i][j][2]->barrier) {
 					painter->setPen(thinRedPen);
 				} else
 					painter->setPen(thinPen);
@@ -176,7 +176,7 @@ void GUIBoard::drawGrid(QPainter* painter) {
 						transform.map(QPoint(i * sL + sL, j * sL + sL)));
 			}
 			if (dynamicState->board.edges[i][j][1]) {
-				if ((dynamicState->board.edges[i][j][1])->hindernis) {
+				if ((dynamicState->board.edges[i][j][1])->barrier) {
 					painter->setPen(thinRedPen);
 				} else
 					painter->setPen(thinPen);
@@ -221,11 +221,11 @@ void GUIBoard::drawRailway(QPainter *painter) {
 			if (lastMove[i])
 				painter->drawLine(
 						transform.map(
-								QPoint(lastMove[i]->first.x * sL,
-										lastMove[i]->first.y * sL)),
+								QPoint(lastMove[i]->first->x * sL,
+										lastMove[i]->first->y * sL)),
 						transform.map(
-								QPoint(lastMove[i]->second.x * sL,
-										lastMove[i]->second.y * sL)));
+								QPoint(lastMove[i]->second->x * sL,
+										lastMove[i]->second->y * sL)));
 	}
 	for (int i = 0; i < MAX_X; i++)
 		for (int j = 0; j < MAX_Y; j++)
@@ -234,17 +234,17 @@ void GUIBoard::drawRailway(QPainter *painter) {
 					if (dynamicState->getFromUserSelectedConnections()[i][j][k]) {
 						const Connection* const current =
 								dynamicState->board.edges[i][j][k];
-						if (current->hindernis)
+						if (current->barrier)
 							painter->setPen(fatRedPen);
 						else
 							painter->setPen(fatGreyPen);
 						painter->drawLine(
 								transform.map(
-										QPoint(current->first.x * sL,
-												current->first.y * sL)),
+										QPoint(current->first->x * sL,
+												current->first->y * sL)),
 								transform.map(
-										QPoint(current->second.x * sL,
-												current->second.y * sL)));
+										QPoint(current->second->x * sL,
+												current->second->y * sL)));
 					}
 				}
 }
@@ -257,12 +257,12 @@ void GUIBoard::drawPawns(QPainter *painter) {
 		painter->setBrush(brush);
 		QPoint point = transform.map(QPoint(pos.x * sL - 18, pos.y * sL - 25));
 		painter->setPen(thinPen);
-        painter->drawRoundedRect(point.x(), point.y(), 10*2, 25*2, 2, 2);
+		painter->drawRoundedRect(point.x(), point.y(), 10 * 2, 25 * 2, 2, 2);
 		if (dynamicState->playerStatus[i->spielerfarbe] != NOT_BANNED) {
 			QString qstring = QString::fromStdString(
 					bannedStatusToString(
 							dynamicState->playerStatus[i->spielerfarbe]));
-            QRect rect(point.x(), point.y(), 10*2, 25*2);
+			QRect rect(point.x(), point.y(), 10 * 2, 25 * 2);
 			painter->setPen(fatPen);
 			painter->setFont(QFont("Times", 10, QFont::Bold));
 			painter->drawText(rect, Qt::AlignCenter, qstring);
@@ -293,12 +293,12 @@ void GUIBoard::drawCityNames(QPainter* painter) {
 	QPixmap* schild = new QPixmap("images/schildkl.gif");
 	const City* const * townList = dynamicState->board.cityList;
 	painter->setPen(fatPen);
-    painter->setFont(QFont("Times", 8*2, QFont::Bold));
+	painter->setFont(QFont("Times", 8 * 2, QFont::Bold));
 	for (int i = 0; i < dynamicState->board.numberCities; i++) {
 		QRect* rect = new QRect(
 				transform.map(
-                        QPoint((townList[i]->x * sL - 200 + 5),
-                                (townList[i]->y * sL + 10))), QSize(800, 100));
+						QPoint((townList[i]->x * sL - 200 + 5),
+								(townList[i]->y * sL + 10))), QSize(800, 100));
 		QRect boundingRect;
 		boundingRect = painter->boundingRect(*rect,
 				Qt::AlignHCenter | Qt::AlignTop,
@@ -307,7 +307,7 @@ void GUIBoard::drawCityNames(QPainter* painter) {
 		boundingRect.setHeight(boundingRect.height());
 		boundingRect.setTopLeft(
 				transform.map(QPoint(townList[i]->x * sL, townList[i]->y * sL))
-                        + QPoint(-boundingRect.width() /2, 16));
+						+ QPoint(-boundingRect.width() / 2, 16));
 		boundingRect.setTop(boundingRect.top() - 1);
 		painter->drawPixmap(boundingRect.topLeft(),
 				schild->scaled(boundingRect.size()));
@@ -319,9 +319,9 @@ void GUIBoard::drawCityNames(QPainter* painter) {
 void GUIBoard::drawHand(QPainter* painter) {
 	QPen coloredPen(fatPen);
 	if (hand) {
-        double size = 101.66*2;
-        painter->setFont(QFont("Times", 10*2, QFont::Bold));
-        QRect positionRect(0, background->height() - 40, size, 40);
+		double size = 101.66 * 2;
+		painter->setFont(QFont("Times", 10 * 2, QFont::Bold));
+		QRect positionRect(0, background->height() - 40, size, 40);
 		//Higlighted Cities
 		for (int i = 0; i < 5; i++) {
 			if (hand[i]) {
@@ -351,21 +351,21 @@ void GUIBoard::drawRat(QPainter *painter) {
 			585. / 16. * (deadLine + 3), 20);
 }
 
-void GUIBoard::drawPoints(QPainter* painter){
-    QRectF bRect (0,272*2, 103*2, 19.2*2);
-    QPen coloredPen(fatPen);
-    painter->setFont(QFont("Times",8*2,QFont::Bold));
-    for (int i=0; i < (int) playerList.size(); i++){
-        QString printString;
-        printString = QString(" %1 (%2) %3")
-                            .arg(QString::fromStdString(playerList[i]->AIname))
-                            .arg(QString::fromStdString(playerList[i]->owner))
-                            .arg(points.get(playerList[i]));
-        coloredPen.setColor(getQColor(playerList[i]->playerColor));
-        painter->setPen(coloredPen);
-        painter->drawText(bRect,Qt::AlignCenter, printString);
-        bRect.moveTop(bRect.top()+19.2*2);
-    }
+void GUIBoard::drawPoints(QPainter* painter) {
+	QRectF bRect(0, 272 * 2, 103 * 2, 19.2 * 2);
+	QPen coloredPen(fatPen);
+	painter->setFont(QFont("Times", 8 * 2, QFont::Bold));
+	for (int i = 0; i < (int) playerList.size(); i++) {
+		QString printString;
+		printString = QString(" %1 (%2) %3").arg(
+				QString::fromStdString(playerList[i]->AIname)).arg(
+				QString::fromStdString(playerList[i]->owner)).arg(
+				points.get(playerList[i]));
+		coloredPen.setColor(getQColor(playerList[i]->playerColor));
+		painter->setPen(coloredPen);
+		painter->drawText(bRect, Qt::AlignCenter, printString);
+		bRect.moveTop(bRect.top() + 19.2 * 2);
+	}
 }
 
 void GUIBoard::drawSelectedCoordinates(QPainter* painter) {
@@ -409,10 +409,10 @@ void GUIBoard::setBoard(const Board* board) {
 	this->board = board;
 }
 void GUIBoard::setDynamicState(const DynamicState* dyState) {
-    if (dynamicState)
-        delete dynamicState;
-    dynamicState = dyState;
-    board = &(dyState->board);
+	if (dynamicState)
+		delete dynamicState;
+	dynamicState = dyState;
+	board = &(dyState->board);
 }
 void GUIBoard::setHand(const City** hand) {
 	this->hand = hand;
